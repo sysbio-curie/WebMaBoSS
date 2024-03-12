476b0bd31d69445141c1431e1032a004e4364ad6
Vincent Noël <contact@vincent-noel.fr>
2024-03-12 11:31:30 -0300

    Using python3 from conda

--- create_default_admin.sh
+++ create_default_admin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+/opt/conda/bin/python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 printf "admin: admin\nadmin_email: admin@example.com\n" >> /var/webmaboss/data/settings/config.yml
--- create_default_db.sh
+++ create_default_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 manage.py makemigrations api -v 2 "$@"
-python3 manage.py migrate -v 2 "$@"
-python3 manage.py collectstatic --noinput "$@" > /dev/null
+/opt/conda/bin/python3 manage.py makemigrations api -v 2 "$@"
+/opt/conda/bin/python3 manage.py migrate -v 2 "$@"
+/opt/conda/bin/python3 manage.py collectstatic --noinput "$@" > /dev/null
 if [ ! -f /var/webmaboss/data/settings/config.yml ]; then
   random_string=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 24`
   printf "secret_key: %s\nallowed_hosts: ['*']\n" "$random_string"> /var/webmaboss/data/settings/config.yml
